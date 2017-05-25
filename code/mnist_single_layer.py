8b2a056ada149f4eec9f0868594159c22092af67
Huang Bo <bo.huang@terraloupe.com>
2017-05-25 16:44:11 +0200

    nengo tutorial

--- code/mnist_single_layer.py
+++ code/mnist_single_layer.py
@@ -1,31 +1,54 @@
 import nengo
 import numpy as np
 
-from keras.datasets import mnist
-from keras.utils import np_utils
+from nengo_extras.data import load_mnist
+from nengo_extras.vision import Gabor, Mask
 
 
+def one_hot(labels, c=None):
+    assert labels.ndim == 1
+    n = labels.shape[0]
+    c = len(np.unique(labels)) if c is None else c
+    y = np.zeros((n, c))
+    y[np.arange(n), labels] = 1
+    return y
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# data pre-processing
-X_train = X_train.reshape(X_train.shape[0], -1) / 255.   # normalize
-X_test = X_test.reshape(X_test.shape[0], -1) / 255.      # normalize
+rng = np.random.RandomState(9)
 
-y_train = np_utils.to_categorical(y_train, nb_classes=10)
-y_test = np_utils.to_categorical(y_test, nb_classes=10)
+
+# --- load the data
+(X_train, y_train), (X_test, y_test) = load_mnist()
+
+X_train = 2 * X_train - 1  # normalize to -1 to 1
+X_test = 2 * X_test - 1  # normalize to -1 to 1
+
+train_targets = one_hot(y_train, 10)
+test_targets = one_hot(y_test, 10)
 
 print X_train.shape
-print y_train.shape
+print train_targets.shape
 
 
 # --- set up network parameters
 input_shape = X_train.shape[1]  # input_shape=784
-output_shape = y_train.shape[1]  # output_shape=10
+output_shape = train_targets.shape[1]  # output_shape=10
 
 
+# n_hid = 300
 n_hid = 1000
+# n_hid = 3000
+
+# encoders = rng.normal(size=(n_hid, 11, 11))
+# encoders = Gabor().generate(n_hid, (11, 11), rng=rng)
+# encoders = Mask((28, 28)).populate(encoders, rng=rng, flatten=True)
+
+# print type(encoders)
+# print "encoder", encoders.shape
+
+
 encoders = np.random.normal(0, 1, size= (1000, 28*28))
+
 
 ens_params = dict(
     eval_points=X_train,
@@ -43,7 +66,7 @@ with nengo.Network(seed=3) as model:
     v = nengo.Node(size_in=output_shape)
     conn = nengo.Connection(
         a, v, synapse=None,
-        eval_points=X_train, function=y_train, solver=solver)
+        eval_points=X_train, function=train_targets, solver=solver)
 
 
 with nengo.Simulator(model) as sim:
@@ -59,4 +82,13 @@ with nengo.Simulator(model) as sim:
     print("Train/test error: %0.2f%%, %0.2f%%" % (train_error, test_error))
 
 
+
+    image = X_train[785,:]
+    image_new = np.reshape(image,(28,28))
+    import matplotlib.pyplot as plt
+    plt.figure()
+    plt.imshow(image_new)
+    plt.show()
+    print "the output", get_outs(image)
+    print "the output is", np.argmax(get_outs(image))
 
