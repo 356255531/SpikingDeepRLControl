a8d2302f5caf8c1cf0ee949b7836eab80bed5c58
Huang Bo <bo.huang@terraloupe.com>
2017-06-03 12:27:42 +0200

    snn training on whole data and do prediction

--- code/snn_network.py
+++ code/snn_network.py
@@ -4,22 +4,22 @@ import numpy as np
 from keras.datasets import mnist
 from keras.utils import np_utils
 from vision import Gabor, Mask
+from sklearn.metrics import accuracy_score
 
 
 class Q_network:
 
-    def __init__(self, input_shape, output_shape, nb_hidden, encoder_file, weights_file):
+    def __init__(self, input_shape, output_shape, nb_hidden, decoder):
         '''
         :param input_shape: the input dimension without batch_size
         :param output_shape: the output dimension without batch_size
         :param nb_hidden: the number of neurons in ensemble
-        :param weights_file: the path to save weights of connection channel
+        :param decoder: the path to save weights of connection channel
         '''
         self.input_shape = input_shape
         self.output_shape = output_shape
         self.nb_hidden = nb_hidden
-        self.weights_file = weights_file
-        self.encoder_file = encoder_file
+        self.decoder = decoder
 
     def encoder_initialization(self, way="default"):
         if way=="random":
@@ -30,48 +30,113 @@ class Q_network:
             encoders = Mask((28, 28)).populate(encoders, rng=rng, flatten=True)
         return encoders
 
-    def train_network(self, train_data, train_targets):
+    def train_network(self, train_data, train_targets, simulation_time=100):
+        '''
+        training the network useing all training data
+        :param train_data: the training input, shape = (nb_samples, dim_samples)
+        :param train_targets: the label or Q values shape=(nbm samples, dim_samples)
+        :param simulation_time: the time to do the simulation, default = 100s
+        :return: 
+        '''
 
         encoders = self.encoder_initialization()
         solver = nengo.solvers.LstsqL2(reg=0.01)
 
         model = nengo.Network(seed=3)
         with model:
-            input = nengo.Ensemble(n_neurons=self.nb_hidden,
-                                   dimensions=self.input_shape,
-                                   neuron_type=nengo.LIFRate(),
-                                   intercepts=nengo.dists.Choice([-0.5]),
-                                   max_rates=nengo.dists.Choice([100]),
-                                   eval_points=train_data,
-                                   encoders=encoders,
-                                   )
+            input_neuron = nengo.Ensemble(n_neurons=self.nb_hidden,
+                                           dimensions=self.input_shape,
+                                           neuron_type=nengo.LIFRate(),
+                                           intercepts=nengo.dists.Choice([-0.5]),
+                                           max_rates=nengo.dists.Choice([100]),
+                                           eval_points=train_data,
+                                           encoders=encoders,
+                                           )
             output = nengo.Node(size_in=self.output_shape)
-            conn = nengo.Connection(input, output,
+            conn = nengo.Connection(input_neuron,
+                                    output,
                                     synapse=None,
                                     eval_points=train_data,
                                     function=train_targets,
                                     solver=solver
                                     )
+            conn_weights = nengo.Probe(conn, 'weights', sample_every=1.0)
 
         with nengo.Simulator(model) as sim:
-            _, acts = nengo.utils.ensemble.tuning_curves(input, sim, inputs=train_data)
-            weights = sim.data[conn].weights.T
-            #result = np.dot(acts, weights)
-            np.save(self.weights_file, weights)
-            np.save(self.encoder_file, encoders.T)
+            sim.run(simulation_time)
 
-    def prediction(self, input):
-        weights = np.load(self.weights_file)
-        encoders = np.load(self.encoder_file)
+        # save the connection weights after training
+        np.save(self.decoder, sim.data[conn_weights][-1].T)
 
-        return np.dot(np.dot(input, encoders), weights)
+
+    def predict(self, input):
+        '''
+        prediction after training, the output will be the corresponding q values
+        :param input: system state and action shape = (dim_sample)
+        :return: the q values
+        '''
+        encoders = self.encoder_initialization()
+
+        try:
+            decoder = np.load(self.decoder)
+        except IOError:
+            decoder = np.zeros((self.nb_hidden, self.output_shape))
+
+        model = nengo.Network(seed=3)
+        with model:
+            input_neuron = nengo.Ensemble(n_neurons=self.nb_hidden,
+                                          dimensions=self.input_shape,
+                                          neuron_type=nengo.LIFRate(),
+                                          intercepts=nengo.dists.Choice([-0.5]),
+                                          max_rates=nengo.dists.Choice([100]),
+                                          encoders=encoders,
+                                          )
+            output = nengo.Node(size_in=self.output_shape)
+            conn = nengo.Connection(input_neuron.neurons,
+                                    output,
+                                    synapse=None,
+                                    transform=decoder.T
+                                    )
+        with nengo.Simulator(model) as sim:
+            _, acts = nengo.utils.ensemble.tuning_curves(input_neuron, sim, inputs=input)
+
+        return np.dot(acts, sim.data[conn].weights.T)
+
+    def acc_calculation(self, test_data, test_label):
+        encoders = self.encoder_initialization()
+
+        model = nengo.Network(seed=3)
+        with model:
+            input_neuron = nengo.Ensemble(n_neurons=self.nb_hidden,
+                                          dimensions=self.input_shape,
+                                          neuron_type=nengo.LIFRate(),
+                                          intercepts=nengo.dists.Choice([-0.5]),
+                                          max_rates=nengo.dists.Choice([100]),
+                                          encoders=encoders,
+                                          )
+            output = nengo.Node(size_in=self.output_shape)
+            conn = nengo.Connection(input_neuron.neurons,
+                                    output,
+                                    synapse=None,
+                                    transform=np.load(self.decoder).T
+                                    )
+        with nengo.Simulator(model) as sim:
+            def get_outs(images):
+                # The tuning curve tells us how each neuron responds to an incoming input signal.
+                _, acts = nengo.utils.ensemble.tuning_curves(input_neuron, sim, inputs=images)
+                return np.dot(acts, sim.data[conn].weights.T)
+
+            def get_error(images, labels):
+                return np.argmax(get_outs(images), axis=1) != labels
+
+            prediction = get_outs(test_data)
+            acc = accuracy_score(np.argmax(test_label, axis=1), np.argmax(prediction, axis=1))
+
+            print "the test error rate is:", acc
 
 
 
 if __name__ == '__main__':
-
-    from keras.datasets import mnist
-    from keras.utils import np_utils
 
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
@@ -81,23 +146,25 @@ if __name__ == '__main__':
     y_train = np_utils.to_categorical(y_train, nb_classes=10)
     y_test = np_utils.to_categorical(y_test, nb_classes=10)
 
-    model = Q_network(input_shape=28*28,
-                      output_shape=10,
-                      nb_hidden=1000,
-                      encoder_file="encoders.npy",
-                      weights_file="weights.npy")
+    model = Q_network(input_shape=28*28, output_shape=10, nb_hidden=1000, decoder="decoder.npy")
 
-    model.train_network(X_train, y_train)
-
-    image = X_test[111,:]
-    image_new = np.reshape(image,(28,28))
-    import matplotlib.pyplot as plt
-    plt.figure()
-    plt.imshow(image_new)
-    plt.show()
+    # training
+    model.train_network(X_train, y_train, simulation_time=1)
 
 
-    print np.argmax(model.prediction(image))
+    # single prediction
+    # image = X_test[123,:]
+    # image_new = np.reshape(image,(28,28))
+    #
+    # import matplotlib.pyplot as plt
+    # plt.figure()
+    # plt.imshow(image_new)
+    # plt.show()
+    #
+    # print "the predict number is:", np.argmax(model.predict(image))
+
+    model.acc_calculation(X_test, y_test)
+
 
 
 
--- huangbo_ws/nengo/transformation.py
+++ huangbo_ws/nengo/transformation.py
@@ -6,15 +6,40 @@ import matplotlib.pyplot as plt
 model = nengo.Network(label="Communications Channel")
 with model:
     # Create an abstract input signal that oscillates as sin(t)
-    sin = nengo.Node(np.sin)
+    # sin = nengo.Node(np.sin)
+    input = nengo.Node(np.array([1, 2, 3]))
+    # sin = nengo.Node([1])
 
     # Create the neuronal ensembles
-    A = nengo.Ensemble(100, dimensions=1)
-    B = nengo.Ensemble(100, dimensions=1)
+    A = nengo.Ensemble(100, dimensions=3)
+    B = nengo.Ensemble(100, dimensions=3)
 
     # Connect the input to the first neuronal ensemble
-    nengo.Connection(sin, A)
+    nengo.Connection(input, A)
 
     # Connect the first neuronal ensemble to the second
     # (this is the communication channel)
-    nengo.Connection(A, B)
+    nengo.Connection(A, B, transform=-1)
+
+with model:
+    sin_probe = nengo.Probe(input)
+    A_probe = nengo.Probe(A, synapse=.01)  # ensemble output
+    B_probe = nengo.Probe(B, synapse=.01)
+
+with nengo.Simulator(model) as sim:
+    sim.run(2)
+
+plt.figure(figsize=(9, 3))
+plt.subplot(1, 3, 1)
+plt.title("Input")
+plt.plot(sim.trange(), sim.data[sin_probe])
+plt.ylim(0, 1.2)
+plt.subplot(1, 3, 2)
+plt.title("A")
+plt.plot(sim.trange(), sim.data[A_probe])
+plt.ylim(0, 1.2)
+plt.subplot(1, 3, 3)
+plt.title("B")
+plt.plot(sim.trange(), sim.data[B_probe])
+plt.ylim(0, 1.2)
+plt.show()
